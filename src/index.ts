d24ad93ad632bfc71083fa428a05133dc5d21c27
Sam Hession <samhession@live.co.uk>
2021-09-16 17:03:29 +0100

    Adds camera support

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,24 @@ const checkIfVegan = (image: string) => {
     }).catch(e => console.warn(e));
     
 }
+
+let video = (document.getElementById('video') as HTMLVideoElement);
+let canvas = (document.getElementById('canvas') as HTMLCanvasElement);
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video!.srcObject = stream;
+        video!.play();
+    });
+}
+
+document.getElementById("snap")!.addEventListener("click", function() {
+    const context = canvas.getContext("2d")!;
+	context.drawImage(video, 0, 0, 640, 480);
+    const image = canvas.toDataURL();
+    pic.src = image;
+    checkIfVegan(image)
+});
