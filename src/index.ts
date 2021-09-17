5496239f864232ace2dbbd794d3edd476823d14c
Sam Hession <samhession@live.co.uk>
2021-09-17 11:45:38 +0100

    Use rear facing camera if possible

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ let canvas = (document.getElementById('canvas') as HTMLCanvasElement);
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment'} }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video!.srcObject = stream;
     });
